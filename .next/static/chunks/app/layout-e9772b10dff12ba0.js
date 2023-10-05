219e2f193363e9b1e5a73886859bc4230685bdad
Tom <tom@0x14.me>
2023-10-06 10:07:16 +1100

    fixed contact page - didnt have background blur

--- .next/static/chunks/app/layout-e9772b10dff12ba0.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5512:function(e,n,t){Promise.resolve().then(t.t.bind(t,4724,23)),Promise.resolve().then(t.t.bind(t,5326,23)),Promise.resolve().then(t.t.bind(t,3054,23))},3054:function(){}},function(e){e.O(0,[877,971,864,744],function(){return e(e.s=5512)}),_N_E=e.O()}]);
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 export default function contact() {
     return (
-        <div className='m-8 text-main'>
-            <h1 className="text-6xl text-main">Contact Me</h1>
-            <p>
-                To get in touch, you can contact me via email at: tom@0x14.me
-            </p>
+        <div className='m-8 text-main my-5 mx-2 rounded border-slate-400 backdrop-blur-sm bg-white/5'>
+            <div className='p-8'>
+                <h1 className="text-6xl text-main">Contact Me</h1>
+                <p className='text-l my-2 max-sm:my-5 leading-loose'>
+                    To get in touch, you can contact me via email at: tom@0x14.me
+                </p>
+            </div>
+
         </div>
 
     )
