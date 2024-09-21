1b5a5de1fabcfc71c4d502a9eabd3f1d0d860d8f
tom / <44159624+tomrowland-dev@users.noreply.github.com>
2024-09-21 11:44:50 +1000

    Update page.js
    Fixed scaling issue on home page.

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="text-main my-10 mx-2 rounded border-slate-400 backdrop-blur-sm bg-white/5">
+    <main className="text-main my-5 mx-2 rounded border-slate-400 backdrop-blur-sm bg-white/5">
       <div className='p-12'>
         <h1 className='text-9xl'>Hi, I'm Tom.</h1>
         <p className='text-4xl my-10'>Im a 18y/o self taught developer from Melbourne</p>
