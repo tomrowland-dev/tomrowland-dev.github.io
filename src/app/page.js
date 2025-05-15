a5d9c3bc7bd20625673941b625dc1364d08368b5
tom / <44159624+tomrowland-dev@users.noreply.github.com>
2025-05-15 18:37:20 +1000

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="text-main my-5 mx-2 rounded border-slate-400 backdrop-blur-sm bg-white/5">
       <div className='p-12'>
         <h1 className='text-9xl'>Hi, I'm Tom.</h1>
-        <p className='text-4xl my-10'>Im a 18y/o self taught developer from Melbourne</p>
+        <p className='text-4xl my-10'>Im a 19y/o self taught developer from Melbourne</p>
       </div>
     </main>
   )
